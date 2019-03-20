3f715b060b454742fb13eb57cf353c614027b72b
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-03-20 13:18:32 -0600

    guided exercise finished

--- server.js
+++ server.js
@@ -1,18 +1,47 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet')
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+//middleware
 
+function bouncer(req, res, next) {
+  res.status(404).json( 'these are not the droids you\'re looking for' )
+}
+
+function teamNamer(req, res, next) {
+  req.team = 'Web XVII'; //middleware can modify the request
+  next(); // go ahead and execute the next middleware/route handler
+}
+
+function oneRing(req, res, next) {
+  const seconds = new Date().getSeconds()
+  if (seconds%3 === 0 ){
+  res.status(403).send('You shall not pass')
+  } else {
+  next()}
+}
+
+// server.use(bouncer)
+server.use(express.json());
+server.use(helmet())
+server.use(teamNamer)
+// server.use(oneRing)
+
+//routing
 server.use('/api/hubs', hubsRouter);
 
-server.get('/', (req, res, next) => {
+server.get('/', restricted, (req, res, next) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome to the Lambda Hubs API</p>
+    <p>Welcome ${req.team} to the Lambda Hubs API</p>
     `);
 });
+
+function restricted(req, res, next) {
+  const password = req.headers.password;
+}
 
 module.exports = server;
